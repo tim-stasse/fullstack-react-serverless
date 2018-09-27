551d91d7a65b335749c0894992d7fa4669c56533
Tim Stasse <tjstasse@agiledigital.com.au>
2018-09-28 09:45:29 +1000

    Fixed flow type

--- client/src/register-service-worker.js
+++ client/src/register-service-worker.js
@@ -93,10 +93,12 @@ function checkValidServiceWorker(swUrl) {
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl)
     .then(response => {
+      const contentType = response.headers.get('content-type');
+
       // Ensure service worker exists, and that we really are getting a JS file.
       if (
         response.status === 404 ||
-        response.headers.get('content-type').indexOf('javascript') === -1
+        (contentType && contentType.indexOf('javascript') === -1)
       ) {
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker &&
