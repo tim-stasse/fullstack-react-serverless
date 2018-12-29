5e8706ac291f21f29cc1e9f774232546a7e489c0
Tim Stasse <tjstasse@agiledigital.com.au>
2018-12-29 21:44:08 +1100

    Refactored create and update user to put user

--- server/src/_schema/resolvers/mutation.js
+++ server/src/_schema/resolvers/mutation.js
@@ -1,8 +1,8 @@
 export const mutation = {
-  createUser: (_, { user }, context) =>
-    context.dataSources.jsonPlaceholder.user.create(user),
-  updateUser: (_, { user }, context) =>
-    context.dataSources.jsonPlaceholder.user.update(user.id, user),
+  putUser: (_, { user }, context) =>
+    user.id
+      ? context.dataSources.jsonPlaceholder.user.update(user.id, user)
+      : context.dataSources.jsonPlaceholder.user.create(user),
   deleteUser: (_, { id }, context) =>
     context.dataSources.jsonPlaceholder.user.delete(id)
 };
--- server/src/_schema/type-defs/inputs/index.js
+++ server/src/_schema/type-defs/inputs/index.js
@@ -1,9 +1,8 @@
 import address from './address.gql';
-import createUser from './create-user.gql';
 import filter from './filter.gql';
 import geo from './geo.gql';
 import pagination from './pagination.gql';
 import sort from './sort.gql';
-import updateUser from './update-user.gql';
+import user from './user.gql';
 
-export { address, createUser, filter, geo, pagination, sort, updateUser };
+export { address, filter, geo, pagination, sort, user };
